67c7483123d1f35d42bf8f93a10c987d397a21be
Nagarjuna_Nallimilli <148531051+Nagarjuna3e@users.noreply.github.com>
2025-12-02 13:52:01 +0530

    Update Problem4.java

--- Problem4.java
+++ Problem4.java
@@ -27,22 +27,23 @@ public class Problem4 {
 	}
 
 	public static void main(String[] args) {
-		int[] numbers = { 1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30 };
+		
 		
 		Scanner sc = new Scanner(System.in);
 		
-//		System.out.println("Enter size of the array");
-//		int size = sc.nextInt();
-//		
-//		int[] arr = new int[size];
-//		
-//		for(int i=0;i<size;i++) {
-//			
-//			arr[i] =sc.nextInt();
-//		}
+		System.out.println("Enter size of the array");
+		int size = sc.nextInt();
+		
+		int[] arr = new int[size];
+		
+		for(int i=0;i<size;i++) {
+			
+			arr[i] =sc.nextInt();
+		}
 
-		Map<Integer, Integer> result = countMultiples(numbers);
+		Map<Integer, Integer> result = countMultiples(arr);
 
 		System.out.println(result);
 	}
 }
+
